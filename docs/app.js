fe9f779aa595471cf2c730f6bf8b701dc8ba9889
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:51:06 +0300

    tunnel: update API_URL to https://tender-panda-40.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://little-pug-29.loca.lt';
+const API_URL = 'https://tender-panda-40.loca.lt';
 
 let currentUser = null;
 
