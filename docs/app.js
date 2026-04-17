f79e660f1640cb613c6af37d6c7aa8a8230c0c8e
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:50:27 +0300

    tunnel: update API_URL to https://wet-grasshopper-48.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://dull-impala-57.loca.lt';
+const API_URL = 'https://wet-grasshopper-48.loca.lt';
 
 let currentUser = null;
 
