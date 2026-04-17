e378bd5ca179395bf8c2e634e0854bf075f52f55
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:41:43 +0300

    tunnel: update API_URL to https://nasty-penguin-82.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://thin-frog-57.loca.lt';
+const API_URL = 'https://nasty-penguin-82.loca.lt';
 
 let currentUser = null;
 
