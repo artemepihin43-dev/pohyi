20ea9bdc0e03ff158b4d73ff69c03fc4f46ad9ed
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:53:02 +0300

    tunnel: update API_URL to https://old-dolphin-52.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://nice-liger-71.loca.lt';
+const API_URL = 'https://old-dolphin-52.loca.lt';
 
 let currentUser = null;
 
