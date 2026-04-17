28ff7eeb33777cc98f631fa91a97921a26bc9067
artemepihin43-dev <artemepihin43@github.com>
2026-04-18 00:32:23 +0300

    tunnel: update API_URL to https://soft-seahorse-56.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://xylivpnbot.loca.lt';
+const API_URL = 'https://soft-seahorse-56.loca.lt';
 
 let currentUser = null;
 
