e4b8136ef153fd475eff249b755ff85f5c7ba22e
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:56:29 +0300

    tunnel: update API_URL to https://serious-mouse-73.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://pink-lizard-32.loca.lt';
+const API_URL = 'https://serious-mouse-73.loca.lt';
 
 let currentUser = null;
 
