95a98e0897180c71347ae30f55c2514c2dacb7ad
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:57:23 +0300

    tunnel: update API_URL to https://odd-elephant-32.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://serious-mouse-73.loca.lt';
+const API_URL = 'https://odd-elephant-32.loca.lt';
 
 let currentUser = null;
 
