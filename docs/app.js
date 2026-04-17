d0ce64952d3feed0e96f3d006b8364dcc8105acb
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:47:32 +0300

    tunnel: update API_URL to https://unlucky-horse-45.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://light-fireant-72.loca.lt';
+const API_URL = 'https://unlucky-horse-45.loca.lt';
 
 let currentUser = null;
 
