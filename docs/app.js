b445590659698e8af57f25eef9dd7d0ece7b5ec8
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:45:35 +0300

    tunnel: update API_URL to https://happy-warthog-5.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://rotten-grasshopper-18.loca.lt';
+const API_URL = 'https://happy-warthog-5.loca.lt';
 
 let currentUser = null;
 
