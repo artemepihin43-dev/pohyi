b306b6dfe297e464a710c9dd3edf0217540025da
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:47:29 +0300

    tunnel: update API_URL to https://pretty-hound-81.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://wicked-mayfly-71.loca.lt';
+const API_URL = 'https://pretty-hound-81.loca.lt';
 
 let currentUser = null;
 
