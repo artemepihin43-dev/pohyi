14078b3a81c4ae2abafeb0f457f56b841990efc5
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:52:54 +0300

    tunnel: update API_URL to https://nice-liger-71.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://neat-dodo-39.loca.lt';
+const API_URL = 'https://nice-liger-71.loca.lt';
 
 let currentUser = null;
 
