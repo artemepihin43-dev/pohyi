0caca86775e98c9330a743c63eec50cd9e5e16d9
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:54:48 +0300

    tunnel: update API_URL to https://great-ape-88.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://bright-kangaroo-48.loca.lt';
+const API_URL = 'https://great-ape-88.loca.lt';
 
 let currentUser = null;
 
