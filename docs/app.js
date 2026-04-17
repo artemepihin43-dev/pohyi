fca648ef9d061d5e7ca1480a75a395df8ef19946
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:46:14 +0300

    tunnel: update API_URL to https://kind-moth-2.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://xylivpnbot.loca.lt';
+const API_URL = 'https://kind-moth-2.loca.lt';
 
 let currentUser = null;
 
