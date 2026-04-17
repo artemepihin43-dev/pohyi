0588ac2d025d5b9c60d3841bee8539e280e4daf3
artemepihin43-dev <artemepihin43@github.com>
2026-04-17 23:54:15 +0300

    tunnel: update API_URL to https://itchy-rabbit-95.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://wonderful-grasshopper-51.loca.lt';
+const API_URL = 'https://itchy-rabbit-95.loca.lt';
 
 let currentUser = null;
 
