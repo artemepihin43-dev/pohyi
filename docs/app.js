c37b3eb3add64da6294e791740a17a9eee9df2fe
artemepihin43-dev <artemepihin43@github.com>
2026-04-18 20:04:43 +0300

    tunnel: update API_URL to https://xylivpnbot.loca.lt

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_URL = 'https://blue-yak-72.loca.lt';
+const API_URL = 'https://xylivpnbot.loca.lt';
 
 let currentUser = null;
 
